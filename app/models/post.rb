bd2c529469dcea9df15fe158dd8a8475ca12b0e5
Julie Evans <juliemevans17@gmail.com>
2022-04-20 15:08:59 -0500

    commit tree 77a7530e94b00d7598917373709b215a1c8eec91

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  validate :clickbait?
+
+  CLICKBAIT = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+  def clickbait?
+    if CLICKBAIT.none? { |x| x.match title }
+      errors.add(:title, "not clickbait-y enough")
+    end
+  end
 end
